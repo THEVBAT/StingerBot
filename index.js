1f7fb81a9ca021d81a56141457cf4502c6a51a8b
THEVBAT <82089750+THEVBAT@users.noreply.github.com>
2021-08-28 14:27:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,17 +10,34 @@ const Kick = require('./commands/kick');
 const Ban = require('./commands/ban');
 const Note = require('./commands/note');
 const Help = require('./commands/help');
+const Melio = require('./commands/melioratif');
+const slashp = require('./commands/slash');
 
 //const queue = new Map();
 
 bot.on('ready', function() {
     console.log("pret");
-    bot.user.setActivity("a (inserer nombre aleatoire) d'utilisateur!!");
+    setInterval(() => {
+        bot.user.setActivity("t/help")
+        setTimeout(() => {
+            bot.user.setActivity("ta dead sa chacale", { type: 'STREAMING', url: 'https://www.twitch.tv/THEVBAT08' })
+        }, 10000);
+        setTimeout(() => {
+            bot.user.setActivity("\"Tu la bouge ta caisse connard!\"", { type: 'STREAMING', url: 'https://www.twitch.tv/THEVBAT08' })
+        }, 20000);
+        setTimeout(() => {
+            bot.user.setActivity("salut", { type: 'PLAYING' })
+        }, 30000);
+        setTimeout(() => {
+            bot.user.setActivity("la soloQ un plaisir", { type: 'STREAMING', url: 'https://www.twitch.tv/THEVBAT08' })
+        }, 40000);
+    }, 50000);
+    slashp.execute(Discord, bot)
 });
 
 bot.on('message',function (message) {
     if (message.author.bot) return;
-    let commandUsed = Warn.parse(message, prefix) || Infractions.parse(message, prefix) || Infobot.parse(message, prefix) || Kick.parse(message, prefix) || Ban.parse(message, prefix) || Note.parse(message, prefix) || Help.parse(message, prefix)
+    let commandUsed = Warn.parse(message, prefix) || Infractions.parse(message, prefix) || Infobot.parse(message, prefix) || Kick.parse(message, prefix) || Ban.parse(message, prefix) || Note.parse(message, prefix) || Help.parse(message, prefix) || Melio.parse(message, prefix)
 });
 
 bot.on('message', async message => {
@@ -56,6 +73,20 @@ bot.on('message', async message => {
             .setColor("0012FF")
             .addField("volume mis a", `${volume * 100}%`)
         message.channel.send(embed);
+    } else if (message.content.toString().toLowerCase().startsWith(`${prefix}ping`)) {
+        let args = message.content.toString().substr(6);
+        if (message.content.toString().toLowerCase().split(' ')[1] == "caisse") bot.user.setActivity("\"Tu la bouge ta caisse connard!\"", { type: 'STREAMING', url: 'https://www.twitch.tv/THEVBAT08' });
+        message.channel.send("ping réussi ma gueule");
+        if (message.content.toString().toLowerCase().split(' ')[1] == "salut") bot.user.setActivity("salut", { type: 'PLAYING' });
+        if (message.content.toString().toLowerCase().split(' ')[1] == "chacale") bot.user.setActivity("ta dead sa chacale", { type: 'STREAMING', url: 'https://www.twitch.tv/THEVBAT08' });
+        if (message.content.toString().toLowerCase().split(' ')[1] == "soloq") bot.user.setActivity("la soloQ un plaisir", { type: 'STREAMING', url: 'https://www.twitch.tv/THEVBAT08' });
+    } else if (message.content.toString().toLowerCase().startsWith (`${prefix}infoping`)) {
+        const embed = new MessageEmbed()
+            .setColor("0012FF")
+            .setTitle("Ping")
+            .setDescription("Tout les type de ping")
+            .addField("ping:", "`t/ping caisse` \n `t/ping salut` \n `t/ping chacale` \n `t/soloq`")
+        message.channel.send(embed)
     }
 });
 
